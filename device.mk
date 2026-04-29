70e9afc7fea9bc3aabaabe823de528e44f67408d
Hongchenguoke <83236469+Hongchenguoke@users.noreply.github.com>
2026-04-29 23:54:08 +0800

    Update device.mk
    Some fixes

--- device.mk
+++ device.mk
@@ -86,8 +86,7 @@ endif
 # ===========================================================
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab \
-    $(DEVICE_PATH)/twrp.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/twrp.fstab \
-    $(DEVICE_PATH)/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
+    $(DEVICE_PATH)/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc \
     $(DEVICE_PATH)/init.recovery.hlthchrg.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.hlthchrg.rc \
     $(DEVICE_PATH)/init.recovery.ldconfig.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.ldconfig.rc \
     $(DEVICE_PATH)/init.recovery.logd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.logd.rc \
